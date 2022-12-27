3c2ef5f0827afd141fb36388a4268a727a41013a
powerfulnexto <aksenenokdanil@yandex.com>
2022-12-27 09:55:09 +0300

    v2.5.1

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ function createtDate(createdAt){
     date.toLocaleString();
 
     createdAt = `
-    ${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}
-    ${date.getHours()}:${date.getMinutes()}
+    ${date.getDate().toString().padStart(2, '0')}.${(date.getMonth()+1).toString().padStart(2, '0')}.${date.getFullYear()}
+    ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}
     `;
 
     return createdAt;
